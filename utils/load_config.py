65b48b77f396d718b24bc7014c6e6a7625635744
Nino Risteski <ninoristeski@gmail.com>
2024-04-11 14:15:03 +0200

    load config 2

--- utils/load_config.py
+++ utils/load_config.py
@@ -10,6 +10,14 @@ load_dotenv()
 
 def __init__(self) -> None:
     with open(here("configs/app_config.yml")) as cfg:
-        app_cofing = yaml.load(ccfg, Loader=yaml.FullLoader)
+        app_cofing = yaml.load(cfg, Loader=yaml.FullLoader)
 
-    
+    # LLM Config
+    self.llm_engine = app_config["llm_config"]["engine"]
+    self.ll_system_role = app_config["llm_config"]["llm_system_role"]
+    self.persist_directory = str(here(
+        app_config["directories"]["persist_directory"]))
+    self.custom_persist_directory = str(here(
+        app_config["directories"]["custom_persist_directory"]))
+    self.embedding_model = OpenAIEmbeddings()
+
